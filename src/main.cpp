e7bd4c37f4fa4ccb01d25efa7cfd6d656c24a474
keegan-patterson <logmein.adachi@gmail.com>
2025-02-21 20:49:53 -0800

    2-21-25 Display letters on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <SFML/Graphics.hpp>
 
+void write_letter(sf::RenderWindow &window, sf::Text &text, const std::string &letter, const sf::Vector2f &position)
+{
+    text.setString(letter);
+    text.setCharacterSize(100);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(position);
+}
+
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
+
+    sf::Font font("C:/Windows/Fonts/arial.ttf");
+    sf::Text text = sf::Text(font);
 
     while (window.isOpen())
     {
@@ -13,9 +24,16 @@ int main()
             {
                 window.close();
             }
+            
+            if(event->is<sf::Event::TextEntered>())
+            {
+                const sf::Event::TextEntered* t_event = event->getIf<sf::Event::TextEntered>();
+                write_letter(window, text, std::string(1, static_cast<char>(t_event->unicode)), {100, 100});
+            }
         }
 
         window.clear();
+        window.draw(text);
         window.display();
     }
 }
