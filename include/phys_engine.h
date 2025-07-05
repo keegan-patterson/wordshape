4335711fc12a2612f4b95f1b0fc0c974bf983b4c
Keegan P <logmein.adachi@gmail.com>
2025-07-04 23:08:07 -0700

    Adding aabbs to physics

--- include/phys_engine.h
+++ include/phys_engine.h
@@ -1,7 +1,32 @@
 #ifndef PHYS_ENGINE_H
 #define PHYS_ENGINE_H
 
-// Function declaration
-void i_do_nothing(); 
+class AABB
+{
+    public:
+    AABB() = default;                            // Default constructor
+    float x_min, y_min; // Minimum x and y coordinates
+    float x_max, y_max; // Maximum x and y coordinates
+};
+
+class PhysEngine
+{
+    bool AABBvsAABB(AABB a, AABB b);
+};
+
+class PhysItem
+{
+    AABB aabb; // Axis aligned bounding box for the item
+
+public:
+    PhysItem() = default;                            // Default constructor
+    PhysItem(const PhysItem &) = default;            // Copy constructor
+    PhysItem(PhysItem &&) = default;                 // Move constructor
+    PhysItem &operator=(const PhysItem &) = default; // Copy assignment operator
+    PhysItem &operator=(PhysItem &&) = default;      // Move assignment operator
+    ~PhysItem() = default;                           // Destructor
+
+    // Additional member functions can be added here
+};
 
 #endif // PHYS_ENGINE_H
--- src/phys_engine.cpp
+++ src/phys_engine.cpp
@@ -1,6 +1,16 @@
 #include "phys_engine.h"
 
-void i_do_nothing() {
-    int x = 0; // This variable is intentionally unused
-    return; // This function intentionally does nothing
+AABB::AABB() : x_min(0), y_min(0), x_max(0), y_max(0) {}
+
+bool PhysEngine::AABBvsAABB(AABB a, AABB b)
+{
+    // Exit with no intersection if found separated along an axis
+    if (a.x_max < b.x_min || a.x_min > b.x_max)
+        return false;
+    if (a.y_max < b.y_min || a.y_min > b.y_max)
+        return false;
+    // No separating axis found, therefore there is at least one overlapping axis
+    return true;
 }
+
+PhysItem::PhysItem() : aabb(AABB()) {}
